cb35df16fc073e299e95df72f718f70b09006917
Eduardo Paiva <eduardopgz@yahoo.com>
2022-07-14 10:41:19 -0300

    finish

---
+++ encontro2.js
@@ -0,0 +1,14 @@
+let listaAlunos = [] //os alunos da sala estariam nesse array
+let count = listaAlunos.length // aqui estaria o numero total de alunos
+
+
+if(count == 0 ){
+    console.log("Quantidade de alunos na sala é 0")
+}
+if( count % 2 == 0){
+    console.log("Quantidade de alunos na sala é par")
+}
+else{
+    console.log("Quantidade de alunos na sala é impar") 
+}
+
